700e04216082ee879fa1a7dca4bc17fc312bec17
Daniel Martínez <39061102+Danie1M@users.noreply.github.com>
2022-08-09 20:07:11 -0500

    Practica extraccion del Json

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,65 @@
 "use strict";
+
+//import { TIMEOUT } from "dns";
+//import { url } from "inspector";
 import * as puppeteer from "puppeteer";
-
-const visitGoogle = async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto("https://www.google.com/");
-    await page.screenshot({path: "googleSS.png"});
-    await page.waitForTimeout(5000);
-    await page.goto("https://developer.mozilla.org/es/");
-    await page.screenshot({path: "developerSS.png"});
-    await page.waitForTimeout(5000);
-    await page.goto("https://pptr.dev/#usage");
-    await page.screenshot({path: "pptrSS.png"});
-
-    await browser.close();
+interface objtData {
+    title: string,
+    image: string,
+    price: string,
+    author: string
 };
 
-visitGoogle();
+const fs = require('fs');
+async function saveJson(objJson) {
+    try {
+        const res2 = await fs.promises.readFile('ejemplo2.json', 'utf8')
+        //console.log("Res2", res2)
+        if (!res2) {
+            await fs.promises.writeFile('ejemplo1.json', JSON.stringify(objJson, null, 2), 'utf8', (err) => {
+                if (err) throw err;
+                console.log('Se guardo correctamenete1');
+            });
+            await fs.promises.writeFile('ejemplo2.json', JSON.stringify(objJson, null, 2), 'utf8', (err) => {
+                if (err) throw err;
+                console.log('Se guardo correctamenete1');
+            });
+        } else {
+            const parRes = JSON.parse(res2)
+            parRes.push(...objJson)
+            fs.writeFile('ejemplo2.json', JSON.stringify(parRes, null, 2), 'utf8', (err) => {
+                if (err) throw err;
+                console.log('Se guardo correctamenete2');
+            });
+        }
+    } catch (err) { console.log("error no jalo la guardada") }
+}
+
+
+const visitGoogle = async (url) => {
+    const browser = await puppeteer.launch({ headless: false, timeout: 120000 });
+    const page = await browser.newPage();
+    await page.goto(url);
+    const cicloPage = await page.evaluate(() => {
+        const res: objtData[] = [];
+        const info = Array.from(document.querySelectorAll('.products-grid.first .item'));
+        for (const objPro of info) {
+            const title = objPro.querySelector('.products-grid.first .item .product-information a').textContent;
+            const image = objPro.querySelector('.products-grid.first .item .product-image img').getAttribute('src');
+            const price = objPro.querySelector('.products-grid.first .item .product-information .price').textContent;
+            const author = objPro.querySelector('.products-grid.first .item .product-information .author').textContent;
+            res.push({ title, image, price, author });
+        }
+        return res
+    })
+    saveJson(cicloPage)
+    await page.waitForTimeout(1000);
+    await browser.close();
+    return cicloPage;
+};
+
+let url = "https://www.tematika.com/libros?limit=40&p="
+for (let x = 1; x <= 5;x++) {
+    visitGoogle(url + `${x}`);
+}
+
